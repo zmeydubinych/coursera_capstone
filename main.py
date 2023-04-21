db1c841052490f4b3e2f702a6e835742bff14b47
Zmeydubinych <ivan.pronin@feltiz.com>
2023-04-21 15:27:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url="https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
+tesla_revenue = pd.read_html(url)[1]
+
+tesla_revenue.columns=['Date','Revenue']
+tesla_revenue['Revenue'] = tesla_revenue['Revenue'].str.replace(',','').str.replace('$','')
+tesla_revenue.drop([52], axis = 0, inplace = True)
+tesla_revenue.sort_values(['Date'], ascending=True, axis=0, inplace=True)
+tesla_revenue.set_index('Date', inplace=True)
+tesla_revenue=tesla_revenue['Revenue'].astype('int')
+print(tesla_revenue.head())
+tesla_revenue.plot(kind='line')
+plt.title('Tesla Revenue 2010-2022 | TSLA')
+plt.xlabel('Date')
+plt.ylabel('Millions of US $')
+plt.show()
+
+
