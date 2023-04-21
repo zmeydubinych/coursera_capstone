9916ec86fa9e46213b77486387ef7c30cc47b937
Zmeydubinych <ivan.pronin@feltiz.com>
2023-04-21 19:20:05 +0300

    last version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -6,15 +7,16 @@ tesla_revenue = pd.read_html(url)[1]
 
 tesla_revenue.columns=['Date','Revenue']
 tesla_revenue['Revenue'] = tesla_revenue['Revenue'].str.replace(',','').str.replace('$','')
-tesla_revenue.drop([52], axis = 0, inplace = True)
+avg_revenue=tesla_revenue['Revenue'].astype('float').mean(axis=0)/200
+tesla_revenue['Revenue'].replace(np.nan, avg_revenue, inplace=True)
+# print(tesla_revenue.head())
 tesla_revenue.sort_values(['Date'], ascending=True, axis=0, inplace=True)
 tesla_revenue.set_index('Date', inplace=True)
 tesla_revenue=tesla_revenue['Revenue'].astype('int')
-print(tesla_revenue.head())
+# print(tesla_revenue.head())
 tesla_revenue.plot(kind='line')
 plt.title('Tesla Revenue 2010-2022 | TSLA')
 plt.xlabel('Date')
 plt.ylabel('Millions of US $')
 plt.show()
-
 
