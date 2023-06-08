ce1701306171a837a788450addb56ee8e963bbcc
zmeydubinych <ivan.pronin@felitz.com>
2023-06-08 09:14:44 +0300

    add new file

--- Dashboard for SpaceX.py
+++ Dashboard for SpaceX.py
@@ -12,10 +12,6 @@ spacex_df = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appd
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
-# filtered_df = spacex_df[spacex_df['Launch Site'] == 'CCAFS LC-40'].groupby(['class']).count().reset_index()
-# filtered_df.rename(columns={'Unnamed: 0': 'Count of result'}, inplace=True)
-# print(filtered_df)
-
 # Create a dash application
 app = dash.Dash(__name__)
 
@@ -85,16 +81,19 @@ def get_pie_chart(entered_site):
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
 @app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
               [Input(component_id='site-dropdown', component_property='value'),
-               Input(component_id="payload-slider", component_property='value')])
+               Input(component_id='payload-slider', component_property='value')])
 
-def get_scatter_chart(entered_site, payload_mass):
-    filtered_df = spacex_df[spacex_df['Launch Site'] == entered_site]
-    # filtered_df.rename(columns={'Unnamed: 0': 'Count of result'}, inplace=True)
+def get_scatter_chart(entered_site, payload):
+    filtered_df = spacex_df[(spacex_df['Launch Site'] == entered_site)]
+    # or (min_payload < spacex_df['Payload Mass (kg)'] < max_payload)
     if entered_site == 'ALL':
-        fig = px.scatter(spacex_df, x='Payload Mass (kg)', y='class', color='Booster Version Category')
+        fig = px.scatter(spacex_df, x='Payload Mass (kg)', y='class', color='Booster Version Category', range_x=payload,
+                         title = 'Correlation between payload and success for all sites'
+        )
         return fig
     else:
-        fig = px.scatter(filtered_df, x='Payload Mass (kg)', y='class', color='Booster Version Category')
+        fig = px.scatter(filtered_df, x='Payload Mass (kg)', y='class', color='Booster Version Category', range_x=payload,
+                         title = 'Correlation between payload and success for site: ' + entered_site)
         return fig
 
 
